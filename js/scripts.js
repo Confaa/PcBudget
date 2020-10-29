c1ef045a92f7ebabac77b80bac45be88f8479d9a
Juan Confalonieri <juanconfaa@gmail.com>
2020-10-28 22:34:36 -0300

    Intento JS

---
+++ js/scripts.js
@@ -0,0 +1,71 @@
+var tope = prompt("Indiquenos su presupuesto: ");
+var totalCompra = 0;
+
+function Presupuesto() {
+    this.producto = [];
+
+    this.agregarproducto = function (producto) {
+        this.producto.push(producto);
+        alert("Agregado al presupuesto");
+    };
+}
+
+function Componente(tip, mar, cant) {
+    this.tipo = tip;
+    this.marca = mar;
+    this.cantidad = cant;
+}
+
+var tipo;
+var marca;
+var cantidad;
+var componente;
+var presupuesto = new Presupuesto();
+var x;
+var decision;
+var sigoCargando = true;
+while (tope >= totalCompra && sigoCargando == true) {
+    x = false;
+    tipo = prompt("Tipo de componente: ");
+    marca = prompt("Marca: ");
+    cantidad = prompt("Unidades: ");
+
+    tipo = tipo.toLowerCase();
+
+    marca = marca.trim();
+    marca = marca.toUpperCase();
+
+    if (marca == "ASUS") {
+        totalCompra = totalCompra + 100 * cantidad;
+    } else if (marca == "MSI") {
+        totalCompra = totalCompra + 90 * cantidad;
+    } else if (marca == "GIGABYTE") {
+        totalCompra = totalCompra + 90 * cantidad;
+    } else {
+        x = true;
+        alert("No se ha encontrado el producto solicitado");
+    }
+    if (totalCompra > tope) {
+        alert("No se pudo cargar el producto solicitado, limite superado");
+        break;
+    }
+    if (x == false) {
+        componente = new Componente(tipo, marca, cantidad);
+        presupuesto.agregarproducto(componente);
+    }
+    do {
+        decision = prompt("Desea agregar algo mas al presupuesto? Y(Si) N(no)");
+        decision = decision.toUpperCase();
+        if (decision == "Y") {
+            sigoCargando = true;
+        } else if (decision == "N") {
+            sigoCargando = false;
+        } else {
+            alert("Opcion incorrecta");
+        }
+    } while (decision != "N" && decision != "Y");
+}
+
+console.log(presupuesto);
+console.log(typeof presupuesto);
+alert("Dinero gastado: " + totalCompra);
