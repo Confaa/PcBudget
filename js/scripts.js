5b42c2c866835d8c97613966b2e4fffd237ce5fa
Juan Confalonieri <juanconfaa@gmail.com>
2020-11-08 02:56:24 -0300

    Agregado mothers.html

--- js/scripts.js
+++ js/scripts.js
@@ -4,31 +4,135 @@ var totalCompra = 0; */
 function Presupuesto() {
     this.producto = [];
 
-    this.agregarproducto = function (item) {
+    this.agregarProducto = function (item) {
         this.producto.push(item);
-        localStorage.setItem("lista", JSON.stringify(this.producto));
+        sessionStorage.setItem("lista", JSON.stringify(this.producto));
         /* alert("Agregado al presupuesto"); */
     };
 
     this.base = function () {
-        if (localStorage.getItem("lista") != null)
-            this.producto = JSON.parse(localStorage.getItem("lista"));
+        if (sessionStorage.getItem("lista") != null)
+            this.producto = JSON.parse(sessionStorage.getItem("lista"));
     };
 }
 
-function Componente(tip, mar, cant) {
+function Componente(tip, mar, mod, pre, cons, comp, codPro) {
     this.tipo = tip;
     this.marca = mar;
-    this.cantidad = cant;
+    this.modelo = mod;
+    this.precio = pre;
+    this.consumo = cons;
+    this.compatibilidad = comp;
+    this.codigoProducto = codPro;
+}
+
+function Comparar(lista, valor) {
+    for (i = 0; i < lista.length; i++) {
+        if (lista[i].codigoProducto == valor) {
+            presupuesto.agregarProducto(lista[i]);
+        }
+    }
+}
+
+var botones = document.getElementsByClassName("agregarPresupuesto");
+
+function agregarEventos(lista) {
+    for (var i = 0; i < lista.length; i++) {
+        lista[i].addEventListener("click", function () {
+            Comparar(baseDatos, lista[i].value);
+        });
+    }
 }
 
 var presupuesto = new Presupuesto();
-var compo = new Componente("mother", "asus", 1);
-
 presupuesto.base();
-presupuesto.agregarproducto(compo);
+agregarEventos(botones);
 
 console.log(presupuesto.producto);
+
+var baseDatos = [
+    {
+        "tipo": "Mother",
+        "marca": "Gigabyte",
+        "modelo": "B460M AORUS PRO (rev. 1.0)",
+        "precio": 599,
+        "consumo": 40,
+        "compatibilidad": "Intel",
+        "codigoProducto": 1000
+    },
+    {
+        "tipo": "Mother",
+        "marca": "Gigabyte",
+        "modelo": "B460M AORUS PRO (rev. 1.0)",
+        "precio": 599,
+        "consumo": 40,
+        "compatibilidad": "Intel",
+        "codigoProducto": 1001
+    },
+    {
+        "tipo": "Mother",
+        "marca": "Gigabyte",
+        "modelo": "B460M AORUS PRO (rev. 1.0)",
+        "precio": 599,
+        "consumo": 40,
+        "compatibilidad": "Intel",
+        "codigoProducto": 1002
+    },
+    {
+        "tipo": "Mother",
+        "marca": "Gigabyte",
+        "modelo": "B460M AORUS PRO (rev. 1.0)",
+        "precio": 599,
+        "consumo": 40,
+        "compatibilidad": "Intel",
+        "codigoProducto": 1003
+    },
+    {
+        "tipo": "Mother",
+        "marca": "Gigabyte",
+        "modelo": "B460M AORUS PRO (rev. 1.0)",
+        "precio": 599,
+        "consumo": 40,
+        "compatibilidad": "Intel",
+        "codigoProducto": 1004
+    },
+    {
+        "tipo": "Mother",
+        "marca": "Gigabyte",
+        "modelo": "B460M AORUS PRO (rev. 1.0)",
+        "precio": 599,
+        "consumo": 40,
+        "compatibilidad": "Intel",
+        "codigoProducto": 1005
+    },
+    {
+        "tipo": "Mother",
+        "marca": "Gigabyte",
+        "modelo": "B460M AORUS PRO (rev. 1.0)",
+        "precio": 599,
+        "consumo": 40,
+        "compatibilidad": "Intel",
+        "codigoProducto": 1006
+    },
+    {
+        "tipo": "Mother",
+        "marca": "Gigabyte",
+        "modelo": "B460M AORUS PRO (rev. 1.0)",
+        "precio": 599,
+        "consumo": 40,
+        "compatibilidad": "Intel",
+        "codigoProducto": 1007
+    },
+    {
+        "tipo": "Mother",
+        "marca": "Gigabyte",
+        "modelo": "B460M AORUS PRO (rev. 1.0)",
+        "precio": 599,
+        "consumo": 40,
+        "compatibilidad": "Intel",
+        "codigoProducto": 1008
+    }
+];
 
 /* var tipo;
 var marca;
